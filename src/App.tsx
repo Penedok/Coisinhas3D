a4b7d45eed35a0a956b20031c2f2bcd8120879a7
Karen Penedo <penedok12@gmail.com>
2023-10-22 11:30:06 -0400

    Coisinhas3D

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h2>Bem vindo a coisinhas 3D
+     </h2>
+    </>
+  )
+}
+
+export default App
